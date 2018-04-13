e5550b264c50f5c34e45b2e42e24ed905aecc85c
Gray Smith <gray.smith00@gmail.com>
2018-04-13 09:55:31 -0600

    configure webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,40 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
-    publicPath: '/'
+    publicPath: '/',
   },
   module: {
     rules: [
       { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
   },
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
+      template: 'app/index.html',
+    }),
   ],
-  mode: "development"
+  mode: 'development',
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  )
+}
+
+module.exports = config;
